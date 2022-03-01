cfc751fb47de821328aea93be5e4672035835968
jaco718v <jaco718v@stud.kea.dk>
2022-03-01 09:31:43 +0100

    Rettet ting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,27 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    Scanner sc = new Scanner(System.in);
 
-    public char caeserCypher(int cypher, char bogstav){
+    public char caesarCypher(int cypherShift, char bogstav){
         char krypteret = bogstav;
         if(bogstav !=' '){
-        int encryption = bogstav - 'A'+cypher;
-        if(encryption>25) {
-            encryption=encryption-26;
+        char encryption = (char)(bogstav+cypherShift);
+        if(encryption>'Z') {
+            encryption=(char)(encryption-26);
+            }
+        else if (encryption<'A'){
+            encryption=(char)(encryption+26);
+            }
+        krypteret = (encryption);
         }
-        else if (encryption<0){
-            encryption=encryption+26;
-        }
-        krypteret = (char)(encryption+'A');}
         return krypteret;
     }
 
-    public StringBuilder caeserCypherString(String inputString, int cypherTal){
+    public StringBuilder caesarCypherString(String inputString, int cypherTal){
         StringBuilder text = new StringBuilder();
         inputString = inputString.toUpperCase(Locale.ROOT);
-        System.out.println("Indtast dit cypher-tal");
-        cypherTal = sc.nextInt();
         for (int i =0;i<inputString.length();i++ ){
-            text.append(caeserCypher(cypherTal,inputString.charAt(i)));
+            text.append(caesarCypher(cypherTal,inputString.charAt(i)));
         }
         return text;
     }
@@ -34,11 +32,12 @@ public class Main {
 
     public static void main(String[] args) {
         Main obj = new Main();
-        System.out.println("Indtast det der skal krypteres/afkrypteres");
-        String inputString = obj.sc.next();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indtast det der skal krypteres/dekrypteres");
+        String inputString = sc.next();
         System.out.println("Indtast cypher tal");
-        int cypherTal = obj.sc.nextInt();
-        StringBuilder text = obj.caeserCypherString(inputString,cypherTal);
+        int cypherTal = sc.nextInt();
+        StringBuilder text = obj.caesarCypherString(inputString,cypherTal);
         System.out.println(text);
     }
 }
